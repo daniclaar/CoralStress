17cb6c8b17e54ffb588755564fa3212f6fc963c6
Evan Fiorenza <eaf226@uw.edu>
2019-03-28 16:00:11 -0700

    Update the app with the map of the supplied coordinates and extract maximum monthly mean

--- coralstress_shinyapp/app.R
+++ coralstress_shinyapp/app.R
@@ -1,30 +1,48 @@
 library(shiny)
+library(ggplot2)
+library(maps)
+library(maptools)
+library(ggmap)
+
+
+
+mapWorld <- borders("world", colour="grey20", fill="grey20")
+
+
+load("NOAA/ct5km_climatology_v3.1_MMM.RData")
+
+
+
+
+
 
 # Define UI ----
 ui <- fluidPage(
-  titlePanel("Coral Stress"),
+  titlePanel("title panel"),
   
   sidebarLayout(
-    sidebarPanel(numericInput(inputId="lat",value="",
-                              label="Latitude (Decimal Degrees)"),
-                 br(),
-                 numericInput(inputId="lon",value="",
-                              label="Longitude (Decimal Degrees)"),
-                 br(),
-                 dateInput(inputId="date",label="Query Date"),
-                 br(),
-                 actionButton(inputId="submit",label="Submit")),
-    mainPanel("Put map here",
-              br(),
-              "DHW Output goes here")
+    sidebarPanel("sidebar panel",
+                 numericInput(inputId = 'lat_in',label = 'Latitude, decimal degrees [-90,90] ',value = 0,min=-90,max=90),
+                 numericInput(inputId = 'lon_in',label = 'Longitude, decimal degrees [-180,180]',value=0,min=-180,max=180),
+                 actionButton(inputId = 'click',label = 'Run')
+    ),
+    mainPanel("main panel",
+              plotOutput('map'),
+              textOutput('mmm_out'))
   )
 )
-
-
 # Define server logic ----
 server <- function(input, output) {
+  location=eventReactive(input$click,{location=data.frame(lat=input$lat_in,long=input$lon_in)})
+  output$map=renderPlot(ggplot(aes(x=long,y=lat),data=location())+mapWorld+geom_point(color='red'))
+
   
-}
+  latIdx <- which(abs(lat-input$lat_in)==min(abs(lat-input$lat_in)))
+  lonIdx <- which(abs(lon-input$lon_in)==min(abs(lon-input$lon_in)))
+  
+  output$mmm_out <- reactiveText(sst_NOAA_clim_full[lonIdx,latIdx])
+  
+  }
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
